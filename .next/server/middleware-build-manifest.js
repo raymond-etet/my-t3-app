e54b9cbf7c3bf2f8ae3504dd50637bbea2563ee3
User <user@example.com>
2025-08-29 04:22:50 +0800

    顶顶顶顶

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -4,21 +4,9 @@ globalThis.__BUILD_MANIFEST = {
   },
   "devFiles": [],
   "ampDevFiles": [],
-  "polyfillFiles": [
-    "static/chunks/a1cc4_next_dist_build_polyfills_polyfill-nomodule.js"
-  ],
+  "polyfillFiles": [],
   "lowPriorityFiles": [],
-  "rootMainFiles": [
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_e3cb5e44._.js",
-    "static/chunks/a1cc4_next_dist_compiled_react-dom_6a28e96d._.js",
-    "static/chunks/a1cc4_next_dist_compiled_next-devtools_index_126725f9.js",
-    "static/chunks/a1cc4_next_dist_compiled_234d6f5b._.js",
-    "static/chunks/a1cc4_next_dist_client_31977d5a._.js",
-    "static/chunks/a1cc4_next_dist_6323265d._.js",
-    "static/chunks/f9ddf_@swc_helpers_cjs_f05a57d4._.js",
-    "static/chunks/my-t3-app_a0ff3932._.js",
-    "static/chunks/turbopack-my-t3-app_7e754432._.js"
-  ],
+  "rootMainFiles": [],
   "ampFirstPages": []
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
